43c3e79d6b46fba8fef41b4cd340188a31c4c5bd
Samuel Lesser <ineedmoreplates@gmail.com>
2020-02-22 23:32:18 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -11,7 +11,7 @@ def print_first_directors_movie_titles
 array1 =
 row_index = 0
 
-while row_index < directors_database[0].length do
+while row_index < directors_database[0][:movies].length do
   column_index = 0
   row_index += 1  
   while column_index < directors_database[0][:movies].length do
